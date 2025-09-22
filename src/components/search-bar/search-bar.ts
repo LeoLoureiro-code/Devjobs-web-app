c5a2720326fb718b0306c75a63e60a766234f671
Leo <leolou.90@gmail.com>
2025-09-21 19:22:38 -0700

    change component name

--- src/components/search-bar/search-bar.ts
+++ src/components/search-bar/search-bar.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-search-bar',
+  selector: 'search-bar',
   imports: [],
   templateUrl: './search-bar.html',
   styleUrl: './search-bar.scss'
