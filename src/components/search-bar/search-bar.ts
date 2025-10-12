ae6def9b316444995eb984f54c26c3014f5abe17
Leo <leolou.90@gmail.com>
2025-10-11 22:49:16 -0700

    add search-bar component

---
+++ src/components/search-bar/search-bar.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'search-bar',
+  imports: [],
+  templateUrl: './search-bar.html',
+  styleUrl: './search-bar.scss'
+})
+export class SearchBar {
+
+}
