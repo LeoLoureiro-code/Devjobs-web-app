3fce4fcc17d69be1831d8e567752021c005d1caa
Leo <leolou.90@gmail.com>
2025-10-12 19:16:47 -0700

    add methos to change themes

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,19 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SearchBar } from "../components/search-bar/search-bar";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SearchBar],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('Entertainment-web-app');
+
+  toggleTheme(isDark: boolean) {
+  document.body.classList.remove('light-theme', 'dark-theme');
+  document.body.classList.add(isDark ? 'dark-theme' : 'light-theme');
+}
+
 }
